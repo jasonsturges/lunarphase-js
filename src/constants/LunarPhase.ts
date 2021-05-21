cbdf18ee8732e6667c4fc37763a2a13eeff38d81
Jason Sturges <jason@jasonsturges.com>
2021-05-21 01:23:54 -0500

    Enumerations

---
+++ src/constants/LunarPhase.ts
@@ -0,0 +1,13 @@
+/**
+ * Enumeration of lunar phases
+ */
+export enum LunarPhase {
+  NEW = "New",
+  WANING_CRESCENT = "Waning Crescent",
+  LAST_QUARTER = "Last Quarter",
+  WANING_GIBBOUS = "Waning Gibbous",
+  FULL = "Full",
+  WAXING_GIBBOUS = "Waxing Gibbous",
+  FIRST_QUARTER = "First Quarter",
+  WAXING_CRESCENT = "Waxing Crescent",
+}
---
+++ src/constants/LunarPhaseEmoji.ts
@@ -0,0 +1,13 @@
+/**
+ * Enumeration of lunar phases as emoji
+ */
+export enum LunarPhaseEmoji {
+  NEW = "🌑",
+  WANING_CRESCENT = "🌘",
+  LAST_QUARTER = "🌗",
+  WANING_GIBBOUS = "🌖",
+  FULL = "🌕",
+  WAXING_GIBBOUS = "🌔",
+  FIRST_QUARTER = "🌓",
+  WAXING_CRESCENT = "🌒",
+}
