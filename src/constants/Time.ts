1da952299b4f3b70373507b5e79e2744cde9ab2f
Jason Sturges <jason@jasonsturges.com>
2021-05-24 00:18:37 -0500

    Documentation

--- src/constants/Time.ts
+++ src/constants/Time.ts
@@ -6,12 +6,15 @@ export module Time {
   export const lunationBaseJulianDay = 2423436.6115277777;
 
   /**
+   * Length of one phase (1/8 of a synodic month) in Earth days.
+   */
+  export const phaseLength: number = 3.69132346322;
+
+  /**
    * Length of one synodic month, or days for the phases to complete a cycle.
    * Time between two identical syzygies, equivalent of 29.53059 Earth days.
    *
    * Based on Mean Synodic Month, 2000 AD mean solar days.
    */
   export const synodicMonthlength: number = 29.53058770576;
-
-  export const phaseLength: number = 3.69132346322;
 }
