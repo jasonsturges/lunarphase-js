037799599708dee5ff80507f87e9be32c988d7fb
Jason Sturges <jason@jasonsturges.com>
2021-05-26 00:45:37 -0500

    Documentation

--- src/constants/Time.js
+++ src/constants/Time.js
@@ -1,16 +1,19 @@
 /**
  * Timestamp epoch, January 1, 1970, in Julian Days.
+ * @type {number}
  */
 export const EPOCH = 2440587.5;
 
 /**
  * Lunation 1 as the first new moon of 1923 at approximately
  * 02:41 UTC, January 17, 1923 per Ernest William Brown's lunar theory.
+ * @type {number}
  */
 export const LUNATION_BASE_JULIAN_DAY = 2423436.6115277777;
 
 /**
  * Length of one phase (1/8 of a synodic month) in Earth days.
+ * @type {number}
  */
 export const PHASE_LENGTH = 3.69132346322;
 
@@ -25,5 +28,6 @@ export const ANOMALISTIC_MONTH = 27.55454988;
  * Time between two identical syzygies, equivalent of 29.53059 Earth days.
  *
  * Based on Mean Synodic Month, 2000 AD mean solar days.
+ * @type {number}
  */
 export const SYNODIC_MONTH = 29.53058770576;
