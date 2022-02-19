2ba0daeedf48708076d535be8043241c8accddd8
Jason Sturges <jason@jasonsturges.com>
2022-02-19 15:07:25 -0600

    Lunar month enumeration

---
+++ src/enum/LunarMonth.js
@@ -0,0 +1,14 @@
+/**
+ * Lunar month, time between two successive syzygies of the
+ * same type: new moons or full moons
+ *
+ * @typedef {string} LunarMonth
+ * @enum {LunarMonth}
+ */
+export const LunarMonth = {
+  ANOMALISTIC: "Anomalistic",
+  DRACONIC: "Draconic",
+  SIDEREAL: "Sidereal",
+  SYNODIC: "Synodic",
+  TROPICAL: "Tropical",
+};
